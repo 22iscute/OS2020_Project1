abda927d573e80d36fb0a2f132e9f9fd4cbf61ee
22iscute <45644687+22iscute@users.noreply.github.com>
2020-04-29 16:34:44 +0800

    Add files via upload

---
+++ setting.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#define GET_TIME 333
+#define PRINTK 334
+
+typedef struct process
+{
+	char name[50];
+	int rt, et;
+	pid_t pid;
+} Process;
+
+void unit();
+int get_next_id(int opt, Process* processes, int N, int *id, int *cur_t, int* last_t);
+int create(Process pro);
+void scheduling(int opt, Process *processes, int N);
