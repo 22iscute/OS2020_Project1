ef47482873038065f4a09e40a5ac807998fcbaac
22iscute <45644687+22iscute@users.noreply.github.com>
2020-05-04 21:04:25 +0800

    Update demo.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -3,8 +3,8 @@
 dmesg -c
 for name in TIME_MEASUREMENT FIFO_1 PSJF_2 RR_3 SJF_4; do
         echo "Processing ${name}..."
-
+        
         ./PJ1 < "OS_PJ1_Test/${name}.txt"
-        dmesg -c | grep Project1
+        dmesg | grep Project1
         sleep 2
 done
